26345390acafcd08b11811a5957bea04c7e8d11c
Nikita Gusakov <gusnkt@gmail.com>
2016-09-21 15:26:56 +0300

    feat: add peerToString util

--- utils.js
+++ utils.js
@@ -13,6 +13,23 @@ export function isGroupPeer(peer: Peer): boolean {
   return peer.type === 'group';
 }
 
+export function peerToString(peer: Peer): string {
+  if (peer.key) {
+    return peer.key;
+  }
+
+  switch (peer.type) {
+    case 'user':
+      return `u${peer.id}`;
+
+    case 'group':
+      return `g${peer.id}`;
+
+    default:
+      return `?${peer.id}`;
+  }
+}
+
 export function isSamePeer(peer1: ?Peer, peer2: ?Peer): boolean {
   if (peer1 && peer2) {
     return peer1.id === peer2.id && peer1.type === peer2.type;
