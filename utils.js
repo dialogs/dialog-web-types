43e65d10d60efbd479462823a40073ccd16c4d23
Nikita Gusakov <gusnkt@gmail.com>
2017-11-12 19:27:18 +0300

    feat: add peer to api cast

--- utils.js
+++ utils.js
@@ -30,6 +30,22 @@ export function peerToString(peer: Peer): string {
   }
 }
 
+export function peerToApi(peer: Peer) {
+  switch (peer.type) {
+    case 'user':
+      return { type: 'private', id: peer.id, strId: null };
+
+    case 'group':
+      return { type: 'group', id: peer.id, strId: null };
+
+    case 'sip':
+      return { type: 'sip', id: peer.id, strId: peer.key };
+
+    default:
+      throw new Error(`Unexpected peer type: ${peer.type}`);
+  }
+}
+
 export function isSamePeer(peer1: ?Peer, peer2: ?Peer): boolean {
   if (peer1 && peer2) {
     return peer1.id === peer2.id && peer1.type === peer2.type;
